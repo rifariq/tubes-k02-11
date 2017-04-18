28d8284d32fb229c149d678959fa1538ae40c775
khudzaifah2302 <khudzaifah2302@gmail.com>
2017-04-18 08:24:05 +0700

    Update menu.pas

--- menu.pas
+++ menu.pas
@@ -1,32 +1,5 @@
 program tubes;
-	
-procedure tulisMenu;
-begin
-	writeln('> menu:');
-	writeln;
-	writeln('> load ');
-	writeln('> login');
-	writeln('> lihatRekening ');
-	writeln('> informasiSldo ');
-	writeln('> lihatAktivitasTransaksi ');
-	writeln('> pembuatanRekening ');
-	writeln('> setoran ')
-	writeln('> penarikan ');
-	writeln('> transfer ');
-	writeln('> pembayaran ');
-	writeln('> pembelian ');
-	writeln('> penutupanRekening ');
-	writeln('> perubahanDataRekening ');
-	writeln('> penambahanAutoDebet ');
-	writeln('> exit ');
-end;
 
-procedure load;
-begin
-
-
-end;
-procedure menu;
 type
 	nilaiTukar = record
 		nilaiKursAsal : longint;
@@ -93,8 +66,55 @@ type
 		password : string;
 		status : string;
 	end;
+	jenisFile = record
+		fileNasabah : array [1..100] of nasabah;
+		fileRekening : array [1..150] of rekening;
+		fileSetoran : array [1..200] of setoran;
+		fileTransfer : array [1..200] of transfer;
+		filePembayaran : array [1..200] of pembayaran;
+		filePembelian : array [1..200] of pembelian;
+		fileNilaiTukar : array [1..200] of nilaiTukar;
+	end;
 
+var
+	fileEx : file of jenisFile;
 
+procedure load(var f : fileEx;);
+var
+	namaFile : string;
+begin
+	write('tuliskan nama file : ');
+	readln(namaFile);
+	assign(f, namaFile);
+
+end;
+
+procedure tulisMenu;
+begin
+	writeln('> menu:');
+	writeln;
+	writeln('> load ');
+	writeln('> login');
+	writeln('> lihatRekening ');
+	writeln('> informasiSldo ');
+	writeln('> lihatAktivitasTransaksi ');
+	writeln('> pembuatanRekening ');
+	writeln('> setoran ')
+	writeln('> penarikan ');
+	writeln('> transfer ');
+	writeln('> pembayaran ');
+	writeln('> pembelian ');
+	writeln('> penutupanRekening ');
+	writeln('> perubahanDataRekening ');
+	writeln('> penambahanAutoDebet ');
+	writeln('> exit ');
+end;
+
+procedure menu;
+begin
+	tulisMenu;
+	
+end;
 {program utama}
 begin
 	menu;{test} 
